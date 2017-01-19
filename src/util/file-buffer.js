9fb24ae12bb109c76e1a3729cb883acb4555f39c
Adrian Rocke <business@adrianrocke.com>
2017-01-19 17:55:09 -0600

    added test video

--- src/util/file-buffer.js
+++ src/util/file-buffer.js
@@ -1,22 +1,22 @@
 export default class FileBuffer {
   constructor(buffer) {
-    this._buffer = buffer;
-    this._view = new DataView(buffer);
+    this._buffer = buffer
+    this._view = new DataView(buffer)
   }
   get byteLength() {
     return this._view.byteLength;
   }
   getByte(offset) {
     if (offset < this.byteLength)
-      return this._view.getUint8(offset);
+      return this._view.getUint8(offset)
     else
-      return null;
+      return null
   }
   getBytes(offset, length) {
-    var bytes = [];
-    for (var i = offset; i < offset + length || i < this.byteLength; i++) {
-      bytes.push(this.getBytes(i));
-    }
-    return bytes;
+    let bytes = []
+    let bound = Math.min(offset + length, this.byteLength)
+    for (let i = offset; i < bound; i++)
+      bytes.push(this._view.getUint8(i))
+    return bytes
   }
 }
