2800a333ee7abe3af961c8137076a25b452d0446
Adrian Rocke <business@adrianrocke.com>
2017-01-31 21:12:43 -0600

    added watch script for unit tests

--- spec/ogg/main.spec.js
+++ spec/ogg/main.spec.js
@@ -2,4 +2,8 @@ describe('Spec', function () {
   it('It should be true.', function () {
     expect(true).toBe(true);
   })
+
+  it('It should be false', function () {
+    expect(false).toBe(false);
+  })
 });
