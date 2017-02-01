7499c4f272a89acb8c56dcfcf3b940217464fcfc
Adrian Rocke <business@adrianrocke.com>
2017-01-31 22:27:36 -0600

    buffer iterator uses a Uint8Array to access buffer

--- spec/util/buffer-iterator.spec.js
+++ spec/util/buffer-iterator.spec.js
@@ -3,36 +3,51 @@ var BufferIterator = require('../../src/js/util/buffer-iterator.js');
 describe('BufferIterator', function () {
   it('It should initialize its properties.', function () {
     var buffer = new ArrayBuffer(10);
-    var view = new DataView(buffer);
-    var iterator = new BufferIterator(view, true);
-    expect(iterator._view).toBe(view);
+    var iterator = new BufferIterator(buffer, {
+      byteOffset: 1,
+      byteLength: 5,
+      littleEndian: true
+    });
+    expect(iterator._array.buffer).toBe(buffer);
+    expect(iterator._array.byteLength).toEqual(5);
+    expect(iterator._array.byteOffset).toEqual(1);
     expect(iterator._pos).toEqual(0);
     expect(iterator._lEndian).toBe(true);
   });
 
-  it('It should require a DataView.', function () {
+  it('It should require an ArrayBuffer.', function () {
     expect(function () {
       new BufferIterator();
-    }).toThrowError('BufferIterator must be initialized with a DataView.');
+    }).toThrowError('First argument to BufferIterator must be an ArrayBuffer');
   });
 
-  it('It should not require littleEndian flag.', function () {
+  it('It should not require any of the options.', function () {
     var buffer = new ArrayBuffer(10);
-    var view = new DataView(buffer);
     var iterator;
     expect(function () {
-      iterator = new BufferIterator(view);
+      iterator = new BufferIterator(buffer);
     }).not.toThrowError();
     expect(iterator._lEndian).toBe(false);
+    expect(iterator._array.byteLength).toEqual(10);
+    expect(iterator._array.byteOffset).toEqual(0);
+  });
+
+  it('It should not allow offset and length combinations that go past the buffer.', function () {
+    var buffer = new ArrayBuffer(2);
+    expect(function () {
+      new BufferIterator(buffer, {
+        byteLength: 3,
+        byteOffset: 1
+      });
+    }).toThrowError('Invalid BufferIterator length');
   });
 
   describe('setLittleEndian', function () {
-    var buffer, view, iterator;
+    var buffer, iterator;
 
-    beforeEach(function () {
+    beforeAll(function () {
       buffer = new ArrayBuffer(10);
-      view = new DataView(buffer);
-      iterator = new BufferIterator(view);
+      iterator = new BufferIterator(buffer);
     });
 
     it('It should return its private variable', function () {
@@ -54,4 +69,63 @@ describe('BufferIterator', function () {
       expect(iterator._lEndian).toBe(true);
     })
   });
+
+  describe('byteLength', function () {
+    var buffer;
+
+    beforeAll(function () {
+      buffer = new ArrayBuffer(10);
+    });
+
+    it('It should return the length of the TypedArray inside the iterator.', function () {
+      var iterator = new BufferIterator(buffer);
+      expect(iterator.byteLength).toEqual(10);
+
+      iterator = new BufferIterator(buffer, {
+        byteLength: 5
+      });
+      expect(iterator.byteLength).toEqual(5);
+    });
+  });
+
+  describe('byteOffset', function () {
+    var buffer;
+
+    beforeAll(function () {
+      buffer = new ArrayBuffer(10);
+    });
+
+    it('It should return the offset of the TypedArray inside the iterator.', function () {
+      var iterator = new BufferIterator(buffer);
+      expect(iterator.byteOffset).toEqual(0);
+
+      iterator = new BufferIterator(buffer, {
+        byteOffset: 5
+      });
+      expect(iterator.byteOffset).toEqual(5);
+    });
+  });
+
+  describe('hasBytes', function () {
+    var buffer, iterator;
+
+    beforeAll(function () {
+      buffer = new ArrayBuffer(10);
+      iterator = new BufferIterator(buffer);
+    });
+
+    it('It should return true if the iterator has not reached the end of the buffer.', function () {
+      iterator._pos = 1;
+      expect(iterator.hasBytes).toBe(true);
+    });
+
+    it('It should return false if the iterator has reached the end of the buffer.', function () {
+      iterator._pos = 10;
+      expect(iterator.hasBytes).toBe(false);
+    });
+  });
+
+  describe('nextByte()', function () {
+    
+  })
 });
--- src/js/util/buffer-iterator.js
+++ src/js/util/buffer-iterator.js
@@ -1,14 +1,41 @@
-function BufferIterator(view, littleEndian) {
-  if (!(view instanceof DataView)) {
-    throw new Error('BufferIterator must be initialized with a DataView.');
+function BufferIterator(buffer, options) {
+  if (!(buffer instanceof ArrayBuffer)) {
+    throw new TypeError('First argument to BufferIterator must be an ArrayBuffer');
   }
 
-  this._view = view;
+  options = options || {};
+
+  // Setup the TypedArray for accessing the binary data.
+  // Default offset is 0.
+  // Default length is the length from the offset to the end of the buffer.
+  var offset = options.byteOffset || 0;
+  var length = options.byteLength || (buffer.byteLength - offset);
+
+  if (offset + length > buffer.byteLength) {
+    throw new RangeError('Invalid BufferIterator length');
+  }
+  this._array = new Uint8Array(buffer, offset, length);
+
   this._pos = 0;
-  this._lEndian = !!littleEndian; // default is false
+  this._lEndian = !!options.littleEndian; // default is false
 };
 
 Object.defineProperties(BufferIterator.prototype, {
+  byteLength: {
+    get: function () {
+      return this._array.byteLength;
+    }
+  },
+  byteOffset: {
+    get: function () {
+      return this._array.byteOffset;
+    }
+  },
+  hasBytes: {
+    get: function () {
+      return this._pos < this.byteLength;
+    }
+  },
   littleEndian: {
     get: function () {
       return this._lEndian;
