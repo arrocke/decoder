af1dcb23024659da839bde3ce1f8e3d6cc409fc1
Adrian Rocke <business@adrianrocke.com>
2017-01-31 19:31:14 -0600

    more webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'decoder.js'
+    filename: 'decoder.js',
+    library: 'decoder',
+    libraryTarget: 'umd'
   }
 };
