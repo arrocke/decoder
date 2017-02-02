fe65ff3206fd9cb9d20158c2d1cfcd03b8d00de5
Adrian Rocke <business@adrianrocke.com>
2017-02-01 18:08:32 -0600

    flushed out byte level methods

--- spec/util/buffer-iterator.spec.js
+++ spec/util/buffer-iterator.spec.js
@@ -1,131 +1,288 @@
 var BufferIterator = require('../../src/js/util/buffer-iterator.js');
 
 describe('BufferIterator', function () {
-  it('It should initialize its properties.', function () {
-    var buffer = new ArrayBuffer(10);
-    var iterator = new BufferIterator(buffer, {
-      byteOffset: 1,
-      byteLength: 5,
-      littleEndian: true
-    });
-    expect(iterator._array.buffer).toBe(buffer);
-    expect(iterator._array.byteLength).toEqual(5);
-    expect(iterator._array.byteOffset).toEqual(1);
-    expect(iterator._pos).toEqual(0);
-    expect(iterator._lEndian).toBe(true);
-  });
-
-  it('It should require an ArrayBuffer.', function () {
-    expect(function () {
-      new BufferIterator();
-    }).toThrowError('First argument to BufferIterator must be an ArrayBuffer');
-  });
-
-  it('It should not require any of the options.', function () {
-    var buffer = new ArrayBuffer(10);
-    var iterator;
-    expect(function () {
-      iterator = new BufferIterator(buffer);
-    }).not.toThrowError();
-    expect(iterator._lEndian).toBe(false);
-    expect(iterator._array.byteLength).toEqual(10);
-    expect(iterator._array.byteOffset).toEqual(0);
-  });
-
-  it('It should not allow offset and length combinations that go past the buffer.', function () {
-    var buffer = new ArrayBuffer(2);
-    expect(function () {
-      new BufferIterator(buffer, {
-        byteLength: 3,
-        byteOffset: 1
+  describe('constructor', function () {
+    it('It should initialize its properties.', function () {
+      var buffer = new ArrayBuffer(10);
+      var iterator = new BufferIterator(buffer, {
+        byteOffset: 1,
+        byteLength: 5,
+        littleEndian: true
       });
-    }).toThrowError('Invalid BufferIterator length');
-  });
-
-  describe('setLittleEndian', function () {
-    var buffer, iterator;
-
-    beforeAll(function () {
-      buffer = new ArrayBuffer(10);
-      iterator = new BufferIterator(buffer);
-    });
-
-    it('It should return its private variable', function () {
-      iterator._lEndian = false;
-      expect(iterator.littleEndian).toBe(false);
-      iterator._lEndian = true;
-      expect(iterator.littleEndian).toBe(true);
-    });
-
-    it('It should be able to be changed to false.', function () {
-      iterator._lEndian = true;
-      iterator.littleEndian = false;
-      expect(iterator._lEndian).toBe(false);
-    })
-
-    it('It should be able to be changed to true.', function () {
-      iterator._lEndian = false;
-      iterator.littleEndian = true;
+      expect(iterator._array.buffer).toBe(buffer);
+      expect(iterator._array.byteLength).toEqual(5);
+      expect(iterator._array.byteOffset).toEqual(1);
+      expect(iterator._pos).toEqual(0);
+      expect(iterator._bitPos).toEqual(0);
       expect(iterator._lEndian).toBe(true);
-    })
-  });
-
-  describe('byteLength', function () {
-    var buffer;
-
-    beforeAll(function () {
-      buffer = new ArrayBuffer(10);
+      expect(iterator._onBoundary).toBe(true);
     });
 
-    it('It should return the length of the TypedArray inside the iterator.', function () {
-      var iterator = new BufferIterator(buffer);
-      expect(iterator.byteLength).toEqual(10);
-
-      iterator = new BufferIterator(buffer, {
-        byteLength: 5
-      });
-      expect(iterator.byteLength).toEqual(5);
-    });
-  });
-
-  describe('byteOffset', function () {
-    var buffer;
-
-    beforeAll(function () {
-      buffer = new ArrayBuffer(10);
+    it('It should require an ArrayBuffer.', function () {
+      expect(function () {
+        new BufferIterator();
+      }).toThrowError(TypeError, 'First argument to BufferIterator must be an ArrayBuffer');
     });
 
-    it('It should return the offset of the TypedArray inside the iterator.', function () {
-      var iterator = new BufferIterator(buffer);
-      expect(iterator.byteOffset).toEqual(0);
+    it('It should not require any of the options.', function () {
+      var buffer = new ArrayBuffer(10);
+      var iterator;
+      expect(function () {
+        iterator = new BufferIterator(buffer);
+      }).not.toThrowError();
+      expect(iterator._lEndian).toBe(false);
+      expect(iterator._array.byteLength).toEqual(10);
+      expect(iterator._array.byteOffset).toEqual(0);
+    });
 
-      iterator = new BufferIterator(buffer, {
-        byteOffset: 5
-      });
-      expect(iterator.byteOffset).toEqual(5);
+    it('It should not allow offset and length combinations that go past the buffer.', function () {
+      var buffer = new ArrayBuffer(2);
+      expect(function () {
+        new BufferIterator(buffer, {
+          byteLength: 3,
+          byteOffset: 1
+        });
+      }).toThrowError(RangeError, 'Invalid BufferIterator length');
     });
   });
 
-  describe('hasBytes', function () {
+  describe('properties', function () {
     var buffer, iterator;
 
-    beforeAll(function () {
+    beforeEach(function () {
       buffer = new ArrayBuffer(10);
+    });
+
+    describe('setLittleEndian', function () {
+      beforeEach(function () {
+        iterator = new BufferIterator(buffer);
+      });
+
+      it('It should return its private variable', function () {
+        iterator._lEndian = false;
+        expect(iterator.littleEndian).toBe(false);
+        iterator._lEndian = true;
+        expect(iterator.littleEndian).toBe(true);
+      });
+
+      it('It should be able to be changed to false.', function () {
+        iterator._lEndian = true;
+        iterator.littleEndian = false;
+        expect(iterator._lEndian).toBe(false);
+      })
+
+      it('It should be able to be changed to true.', function () {
+        iterator._lEndian = false;
+        iterator.littleEndian = true;
+        expect(iterator._lEndian).toBe(true);
+      })
+    });
+
+    describe('byteLength', function () {
+      it('It should return the length of the TypedArray inside the iterator.', function () {
+        var iterator = new BufferIterator(buffer);
+        expect(iterator.byteLength).toEqual(10);
+
+        iterator = new BufferIterator(buffer, {
+          byteLength: 5
+        });
+        expect(iterator.byteLength).toEqual(5);
+      });
+    });
+
+    describe('byteOffset', function () {
+      it('It should return the offset of the TypedArray inside the iterator.', function () {
+        var iterator = new BufferIterator(buffer);
+        expect(iterator.byteOffset).toEqual(0);
+
+        iterator = new BufferIterator(buffer, {
+          byteOffset: 5
+        });
+        expect(iterator.byteOffset).toEqual(5);
+      });
+    });
+
+    describe('hasBytes', function () {
+      beforeAll(function () {
+        iterator = new BufferIterator(buffer);
+      });
+
+      it('It should return true if the iterator has not reached the end of the buffer.', function () {
+        iterator._pos = 1;
+        expect(iterator.hasBytes).toBe(true);
+      });
+
+      it('It should return false if the iterator has reached the end of the buffer.', function () {
+        iterator._pos = 10;
+        expect(iterator.hasBytes).toBe(false);
+      });
+    });
+  });
+
+  describe('methods', function () {
+    var buffer, array, iterator;
+
+    beforeEach(function () {
+      buffer = new ArrayBuffer(10);
+      array = new Uint8Array(buffer);
+      for (var i = 0; i < array.byteLength; i++) {
+        array[i] = Math.floor(Math.random() * 256);
+      }
       iterator = new BufferIterator(buffer);
     });
 
-    it('It should return true if the iterator has not reached the end of the buffer.', function () {
-      iterator._pos = 1;
-      expect(iterator.hasBytes).toBe(true);
+    describe('nextByte()', function () {
+      it('It returns the correct value of the byte.', function () {
+        iterator._pos = 1;
+        expect(iterator.nextByte()).toEqual(array[1]);
+      });
+
+      it('It increments the position of the iterator.', function () {
+        iterator._pos = 1;
+        iterator.nextByte();
+        expect(iterator._pos).toEqual(2);
+      });
+
+      it('It cannot read past the end of the buffer.', function () {
+        iterator._pos = 10;
+        expect(function () {
+          iterator.nextByte();
+        }).toThrowError(RangeError, 'Offset is outside the bounds of the BufferIterator');
+      });
+
+      it('It cannot read a byte when the position is off the byte boundary.', function () {
+        iterator._pos = 1;
+        iterator._bitPos = 1;
+        expect(function () {
+          iterator.nextByte();
+        }).toThrowError('BufferIterator is off the byte boundary');
+      });
     });
 
-    it('It should return false if the iterator has reached the end of the buffer.', function () {
-      iterator._pos = 10;
-      expect(iterator.hasBytes).toBe(false);
+    describe('nextBytes()', function () {
+      it('It returns the correct value of the bytes.', function () {
+        iterator._pos = 1;
+        iterator._lEndian = false;
+        expect(iterator.nextBytes(2)).toEqual(array[1] * 256 + array[2]);
+      });
+
+      it('It returns the correct value of the bytes if the little endian flag is set.', function () {
+        iterator._pos = 1;
+        iterator._lEndian = true;
+        expect(iterator.nextBytes(2)).toEqual(array[2] * 256 + array[1]);
+      });
+
+      it('It increments the position of the iterator.', function () {
+        iterator._pos = 1;
+        iterator.nextBytes(2);
+        expect(iterator._pos).toEqual(3);
+      });
+
+      it('It can only read positive integer amounts of bytes.', function () {
+        iterator._pos = 1;
+        expect(function () {
+          iterator.nextBytes('test')
+        }).toThrowError(TypeError, 'First argument for nextBytes must be a positive integer less than 6');
+      });
+
+      it('It cannot safely read more than 6 bytes, as that is the maximum safe size of an integer.', function () {
+        iterator._pos = 1;
+        expect(function () {
+          iterator.nextBytes(7)
+        }).toThrowError(TypeError, 'First argument for nextBytes must be a positive integer less than 6');
+      });
+
+      it('It cannot read past the end of the buffer.', function () {
+        iterator._pos = 8;
+        expect(function () {
+          iterator.nextBytes(3);
+        }).toThrowError(RangeError, 'Offset is outside the bounds of the BufferIterator');
+      });
+
+      it('It cannot read a byte when the position is off the byte boundary.', function () {
+        iterator._pos = 1;
+        iterator._bitPos = 1;
+        expect(function () {
+          iterator.nextBytes(2);
+        }).toThrowError('BufferIterator is off the byte boundary');
+      });
+    });
+
+    describe('nextBytesAsBufferIterator()', function () {
+      it('It returns a BufferIterator with the correct byteOffset and byteLength', function () {
+        iterator._pos = 1;
+        var newIterator = iterator.nextBytesAsBufferIterator(8);
+        expect(newIterator._array.buffer).toBe(iterator._array.buffer);
+        expect(newIterator.byteOffset).toEqual(1);
+        expect(newIterator.byteLength).toEqual(8);
+        expect(newIterator.littleEndian).toBe(false);
+      });
+
+      it('It increments the position of the iterator.', function () {
+        iterator._pos = 1;
+        iterator.nextBytesAsBufferIterator(8);
+        expect(iterator._pos).toEqual(9);
+      });
+
+      it('It can only read positive integer amounts of bytes.', function () {
+        iterator._pos = 1;
+        expect(function () {
+          iterator.nextBytesAsBufferIterator('test')
+        }).toThrowError(TypeError, 'First argument for nextBytesAsBufferIterator must be a positive integer');
+      });
+
+      it('It cannot read past the end of the buffer.', function () {
+        iterator._pos = 8;
+        expect(function () {
+          iterator.nextBytesAsBufferIterator(3);
+        }).toThrowError(RangeError, 'Offset is outside the bounds of the BufferIterator');
+      });
+
+      it('It cannot read a byte when the position is off the byte boundary.', function () {
+        iterator._pos = 1;
+        iterator._bitPos = 1;
+        expect(function () {
+          iterator.nextBytesAsBufferIterator(2);
+        }).toThrowError('BufferIterator is off the byte boundary');
+      });
+    });
+
+    describe('nextBits()', function () {
+      it ('It returns the correct value of the bits', function () {
+        this._pos = 1;
+        this._bitPos = 3;
+        var expected = ((array[1] & 0b111111) << 3) + ((array[2] & 0b11100000) >>> 5)
+        expect(iterator.nextBits(9)).toEqual(expected);
+      });
+
+      it('It increments the position of the iterator.', function () {
+        this._pos = 1;
+        this._bitPos = 3;
+        iterator.nextBits(9);
+        expect(iterator._pos).toEqual(2);
+        expect(iterator._bitPos).toEqual(4);
+      });
+
+      it('It can only read positive integer amounts of bits.', function () {
+        iterator._pos = 1;
+        expect(function () {
+          iterator.nextBits('test')
+        }).toThrowError(TypeError, 'First argument for nextBits must be a positive integer less than 52');
+      });
+
+      it('It cannot safely read more than 52 bits, as that is the maximum safe size of an integer.', function () {
+        iterator._pos = 1;
+        expect(function () {
+          iterator.nextBits(53)
+        }).toThrowError(TypeError, 'First argument for nextBits must be a positive integer less than 52');
+      });
+
+      it('It cannot read past the end of the buffer.', function () {
+        iterator._pos = 9;
+        expect(function () {
+          iterator.nextBits(9);
+        }).toThrowError(RangeError, 'Offset is outside the bounds of the BufferIterator');
+      });
     });
   });
-
-  describe('nextByte()', function () {
-    
-  })
 });
--- src/js/util/buffer-iterator.js
+++ src/js/util/buffer-iterator.js
@@ -17,7 +17,9 @@ function BufferIterator(buffer, options) {
   this._array = new Uint8Array(buffer, offset, length);
 
   this._pos = 0;
+  this._bitPos = 0;
   this._lEndian = !!options.littleEndian; // default is false
+  this._onBoundary = true;
 };
 
 Object.defineProperties(BufferIterator.prototype, {
@@ -44,6 +46,73 @@ Object.defineProperties(BufferIterator.prototype, {
       this._lEndian = !!val;
     }
   }
-})
+});
+
+BufferIterator.prototype.nextByte = function () {
+  if (this._bitPos !== 0) {
+    throw new Error('BufferIterator is off the byte boundary')
+  }
+  if (this._pos >= this.byteLength) {
+    throw new RangeError('Offset is outside the bounds of the BufferIterator');
+  }
+
+  return this._array[this._pos++];
+}
+
+BufferIterator.prototype.nextBytes = function (n) {
+  if (this._bitPos !== 0) {
+    throw new Error('BufferIterator is off the byte boundary')
+  }
+  if (n < 1 || n > 6 || n % 1 !== 0) {
+    throw new TypeError('First argument for nextBytes must be a positive integer less than 6');
+  }
+  if (this._pos + n > this.byteLength) {
+    throw new RangeError('Offset is outside the bounds of the BufferIterator');
+  }
+
+  var val = 0;
+  if (this.littleEndian) {
+    for (var i = 0; i < n; i++) {
+      val += Math.pow(256, i) * this._array[this._pos++];
+    }
+  }
+  else {
+    for (var i = 0; i < n; i++) {
+      val *= 256;
+      val += this._array[this._pos++];
+    }
+  }
+
+  return val;
+}
+
+BufferIterator.prototype.nextBytesAsBufferIterator = function (n) {
+  if (this._bitPos !== 0) {
+    throw new Error('BufferIterator is off the byte boundary')
+  }
+  if (n < 1 || n % 1 !== 0) {
+    throw new TypeError('First argument for nextBytesAsBufferIterator must be a positive integer');
+  }
+  if (this._pos + n > this.byteLength) {
+    throw new RangeError('Offset is outside the bounds of the BufferIterator');
+  }
+
+  var newIterator = new BufferIterator(this._array.buffer, {
+    byteOffset: this.byteOffset + this._pos,
+    byteLength: n,
+  });
+  this._pos += n;
+  return newIterator;
+};
+
+BufferIterator.prototype.nextBits = function (n) {
+  if (n < 1 || n > 52 || n % 1 !== 0) {
+    throw new TypeError('First argument for nextBits must be a positive integer less than 52');
+  }
+  if (this._pos + (this._bitPos + n) / 8 > this.byteLength) {
+    throw new RangeError('Offset is outside the bounds of the BufferIterator');
+  }
+
+};
 
 module.exports = BufferIterator;
